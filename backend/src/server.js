2c5ae170373e5c9603125872d951243dad080cec
NidhiChaukiyal27 <nidhichaukiyal789@gmail.com>
2026-06-11 17:22:17 +0530

    Configure Express server

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+
+
+//1. Import dependencies
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+//2. Load environment variables
+dotenv.config();
+//3. Create Express app
+const app = express();
+//4. Register middleware
+app.use(express.json());     //JSON parsing
+app.use(cors());             //Frontend-Backend communication
+app.use(helmet());           //Security Headers
+app.use(morgan("dev"));      //Logging
+
+
+//5. Add a health-check route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Hospital Inventory API Running"
+  });
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
